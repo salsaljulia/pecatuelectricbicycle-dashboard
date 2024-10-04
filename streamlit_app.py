bd5d9118f46cb5a61b855f5c6fcb0f006ee7b835
Salsa Julia Jasmine <157215930+salsaljulia@users.noreply.github.com>
2024-10-04 13:41:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
 
+from babel.numbers import format_currency
 hourly_data = pd.read_csv('hour.csv')
 daily_data = pd.read_csv('day.csv') 
 
