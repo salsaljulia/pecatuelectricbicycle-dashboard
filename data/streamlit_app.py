46a04d0813b468934cdbf5d2f9e6e7389b4b2e87
Salsa Julia Jasmine <157215930+salsaljulia@users.noreply.github.com>
2024-10-04 14:35:23 +0700

    Update streamlit_app.py

--- data/streamlit_app.py
+++ data/streamlit_app.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+from babel.numbers import format_currency
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
