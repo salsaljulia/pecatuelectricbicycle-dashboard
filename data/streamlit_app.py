c6c4a83acc2cb409f1128bb7584f7b94d5911abb
Salsa Julia Jasmine <157215930+salsaljulia@users.noreply.github.com>
2024-10-04 14:49:33 +0700

    Update streamlit_app.py

--- data/streamlit_app.py
+++ data/streamlit_app.py
@@ -1,8 +1,10 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
-from babel.numbers import format_currency
+import pandas as pd  # untuk manipulasi data dan pengolahan data tabular
+import numpy as np  # untuk operasi numerik dan array
+import matplotlib.pyplot as plt  # untuk membuat grafik dan visualisasi
+import seaborn as sns  # untuk visualisasi data yang lebih informatif dan menarik
+from sklearn.model_selection import train_test_split  # untuk membagi data menjadi set pelatihan dan pengujian
+from sklearn.linear_model import LinearRegression  # untuk menggunakan model regresi linier
+from sklearn.metrics import mean_squared_error, r2_score  # untuk menghitung metrik evaluasi model
 
 # Set visual style
 sns.set(style='dark')
