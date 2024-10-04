b702c21b4c592a654300f926679d54d670d130ba
Salsa Julia Jasmine <157215930+salsaljulia@users.noreply.github.com>
2024-10-04 14:20:42 +0700

    Update streamlit_app.py

--- data/streamlit_app.py
+++ data/streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-pip install streamlit pandas matplotlib seaborn scikit-learn
+streamlit run streamlit_app.py
 
 # Set title of the app
 st.title("Pecatu Electric Bicycle Rental Dashboard")
